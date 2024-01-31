5aecdd3f052a632692fb4780377619b4106fa165
Manu M Benakal <97689990+Manubenakal@users.noreply.github.com>
2024-01-31 21:34:21 +0530

    Time: 14 ms (46.47%), Space: 17.3 MB (8.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int> hashmap;
-        vector<int> ans;
-        priority_queue<int> maxHeap;
-        for (auto &num:nums) {
-            hashmap[num]++;
+        unordered_map<int, int> counts;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> min_heap;
+        for(auto i : nums) {
+            counts[i]++;
         }
-        for (auto &it:hashmap) {
-            maxHeap.push(it.second);
-        }
-        while (k>0) {
-            int countFreq = maxHeap.top();
-            maxHeap.pop();
-            for (auto &it:hashmap) {
-                if(countFreq == it.second) {
-                    ans.push_back(it.first);
-                    hashmap.erase(it.first);
-                    k--;
-                    break;
-                }
+        
+        for(auto & i : counts) {
+            min_heap.push({i.second, i.first});
+            if(min_heap.size() > k) {
+                min_heap.pop();
             }
         }
-        return ans;
-
+        
+        vector<int> res;
+         while(k--)
+        {
+            res.push_back(min_heap.top().second);
+            min_heap.pop();
+        }
+        return res;
     }
 };
