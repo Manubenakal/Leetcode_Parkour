318f98798238256ad976f721321349e337f22521
Manu M Benakal <97689990+Manubenakal@users.noreply.github.com>
2025-02-19 23:25:07 +0530

    Time: 0 ms (100%), Space: 13.2 MB (97.75%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int low = 0;
-        int high = matrix[0].size()-1;
-
-       while(low < matrix.size() && high > -1){
-               if(target == matrix[low][high]){
-                   return true;
-               }
-               if(target < matrix[low][high]){
-                   high--;
-               }
-               else {
-                   low++;
-               }
-            
-        }
-        return false;
         
+      int l = 0;
+      int h = matrix[0].size() -1;
+
+    
+
+      while(l < matrix.size()  && h > -1){
+
+        if(matrix[l][h] == target){
+            return true;
+        }
+        else if( matrix[l][h] > target){
+        h--;
+
+
+        }
+        else {
+
+l++;
+        }
+
+      }
+
+        return false;
     }
 };
